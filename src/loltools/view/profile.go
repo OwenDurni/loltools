61e784dad08c37e4e285b344cf2d9840a421ecaa
Owen Durni <owen.durni@gmail.com>
2013-11-29 12:01:30 -0500

    directory restructuring

---
+++ src/loltools/view/profile.go
@@ -0,0 +1,14 @@
+package profile
+
+import (
+  "loltools/view"
+  "template/html"
+)
+
+func EditHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func ViewHandler(w http.ResponseWriter, r *http.Request) {
+
+}
