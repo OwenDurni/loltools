2581f78115434dac7b74ba588208dfb3c540a1f0
Owen Durni <owen.durni@gmail.com>
2014-01-13 21:56:24 -0500

    added structs for recent game history stats riot api

---
+++ riot/game.go
@@ -0,0 +1,141 @@
+package riot
+
+// v1.3: http://developer.riotgames.com/api/methods#!/339/1143
+type RecentGamesDto struct {
+  Games      []GameDto `json:"games"`
+  SummonerId int64     `json:"summonerId"`
+}
+
+// v1.3: http://developer.riotgames.com/api/methods#!/339/1143
+type GameDto struct {
+  GameId     int64    `json:"gameId"`
+  MapId      int      `json:"mapId"`
+  CreateDate RiotTime `json:"createDate"`
+
+  GameMode string `json:"gameMode"`
+  GameType string `json:"gameType"`
+  SubType  string `json:"subType"`
+
+  TeamId        int         `json:"teamId"`
+  FellowPlayers []PlayerDto `json:"fellowPlayers"`
+
+  ChampionId     int         `json:"championId"`
+  Level          int         `json:"level"`
+  SummonerSpell1 int         `json:"spell1"`
+  SummonerSpell2 int         `json:"spell2"`
+  Stats          RawStatsDto `json:"stats"`
+
+  Invalid bool `json:"invalid"`
+}
+
+// v1.3: http://developer.riotgames.com/api/methods#!/339/1143
+type PlayerDto struct {
+  ChampionId int   `json:"championId"`
+  SummonerId int64 `json:"summonerId"`
+  TeamId     int   `json:"teamId"`
+}
+
+// v1.3: http://developer.riotgames.com/api/methods#!/339/1143
+type RawStatsDto struct {
+  // Overall Game Stats
+  Win        bool `json:"win"`
+  TimePlayed int  `json:"timePlayed"`
+  Team       int  `json:"team"`
+
+  // KDA
+  ChampionsKilled int `json:"championsKilled"`
+  NumDeaths       int `json:"numDeaths"`
+  Assists         int `json:"assists"`
+  TurretsKilled   int `json:"turretsKilled"`
+  BarracksKilled  int `json:"barracksKilled"`
+
+  // Gold/XP
+  Level      int `json:"level"`
+  Gold       int `json:"gold"`
+  GoldEarned int `json:"goldEarned"`
+  GoldSpent  int `json:"goldSpent"`
+
+  // CS
+  MinionsDenied                   int `json:"minionsDenied"`
+  MinionsKilled                   int `json:"minionsKilled"`
+  NeutralMinionsKilled            int `json:"neutralMinionsKilled"`
+  NeutralMinionsKilledEnemyJungle int `json:"neutralMinionsKilledEnemyJungle"`
+  NeutralMinionsKilledYourJungle  int `json:"neutralMinionsKilledYourJungle"`
+  SuperMonstersKilled             int `json:"superMonsterKilled"`
+
+  // Items
+  Item0                 int `json:"item0"`
+  Item1                 int `json:"item1"`
+  Item2                 int `json:"item2"`
+  Item3                 int `json:"item3"`
+  Item4                 int `json:"item4"`
+  Item5                 int `json:"item5"`
+  Item6                 int `json:"item6"`
+  ItemsPurchased        int `json:"itemsPurchased"`
+  ConsumablesPurchased  int `json:"consumablesPurchased"`
+  NumItemsBought        int `json:"numItemsBought"`
+  LegendaryItemsCreated int `json:"legendaryItemsCreated"`
+
+  // Vision
+  WardPlaced        int `json:"wardPlaced"`
+  SightWardsBought  int `json:"sightWardsBought"`
+  VisionWardsBought int `json:"visionWardsBought"`
+  WardKilled        int `json:"wardKilled"`
+
+  // Damage Dealt
+  DamageDealtPlayer              int `json:"damageDealtPlayer"`
+  TotalDamageDealt               int `json:"totalDamageDealt"`
+  TotalDamageDealtToChampions    int `json:"totalDamageDealtToChampions"`
+  PhysicalDamageDealtPlayer      int `json:"physicalDamageDealtPlayer"`
+  PhysicalDamageDealtToChampions int `json:"physicalDamageDealtToChampions"`
+  MagicDamageDealtPlayer         int `json:"magicDamageDealtPlayer"`
+  MagicDamageDealtToChampions    int `json:"magicDamageDealtToChampions"`
+  TrueDamageDealtPlayer          int `json:"trueDamageDealtPlayer"`
+  TrueDamageDealtToChampions     int `json:"trueDamageDealtToChampions"`
+
+  // Damage Taken
+  TotalDamageTaken    int `json:"totalDamageTaken"`
+  PhysicalDamageTaken int `json:"physicalDamageTaken"`
+  MagicDamageTaken    int `json:"magicDamageTaken"`
+  TrueDamageTaken     int `json:"trueDamageTaken"`
+
+  // Misc Dealt Stats
+  TotalTimeCrowdCotrolDealt int `json:"totalTimeCrowdControlDealt"`
+  TotalHeal                 int `json:"totalHeal"`
+  TotalUnitsHealed          int `json:"totalUnitsHealed"`
+
+  // Vanity stats.
+  KillingSprees         int  `json:"killingSprees"`
+  LargestKillingSpree   int  `json:"largestKillingSpree"`
+  LargestMultiKill      int  `json:"largestMultiKill"`
+  DoubleKills           int  `json:"doubleKills"`
+  TripleKills           int  `json:"tripleKills"`
+  QuadraKills           int  `json:"quadraKills"`
+  PentaKills            int  `json:"pentaKills"`
+  UnrealKills           int  `json:"unrealKills"`
+  NexusKilled           bool `json:"nexusKilled"`
+  FirstBlood            int  `json:"firstBlood"`
+  LargestCriticalStrike int  `json:"largestCriticalStrike"`
+
+  // Number of times various spells were cast.
+  Spell1Cast         int `json:"spell1Cast"`
+  Spell2Cast         int `json:"spell2Cast"`
+  Spell3Cast         int `json:"spell3Cast"`
+  Spell4Cast         int `json:"spell4Cast"`
+  SummonerSpell1Cast int `json:"summonSpell1Cast"`
+  SummonerSpell2Cast int `json:"summonSpell2Cast"`
+
+  // Dominion
+  VictoryPointTotal    int `json:"victoryPointTotal"`
+  NodeCapture          int `json:"nodeCapture"`
+  NodeCaptureAssist    int `json:"nodeCaptureAssist"`
+  NodeNeutralize       int `json:"nodeNeutralize"`
+  NodeNeutralizeAssist int `json:"nodeNeutralizeAssist"`
+
+  // ???
+  TotalPlayerScore     int `json:"totalPlayerScore"`
+  TotalScoreRank       int `json:"totalScoreRank"`
+  CombatPlayerScore    int `json:"combatPlayerScore"`
+  ObjectivePlayerScore int `json:"objectivePlayerScore"`
+  TeamObjective        int `json:"teamObjective"`
+}
